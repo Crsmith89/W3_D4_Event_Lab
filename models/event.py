94638133a8b31bc3b8f4bf979f76ae52b2b6ba42
Craig Smith <craig_ag_smith@outlook.com>
2021-07-01 14:38:51 +0100

    event data added

---
+++ models/event.py
@@ -0,0 +1,9 @@
+class Event():
+    
+    def __init__(self, event_date, event_name, guest_total, room_location, event_description):
+        self.event_date = event_date
+        self.event_name = event_name
+        self.guest_total = guest_total
+        self.room_location = room_location
+        self.event_description = event_description
+        
---
+++ models/event_list.py
@@ -0,0 +1,10 @@
+from models.event import Event
+
+
+
+event1 = Event("14th Feb 2021", "Valentines Day", "20", "Bedroom", "Wear Red, Less is Definitely More")
+event2 = Event("10th July 2021", "Wedding", "200", "The Church", "White Wedding")
+event3 = Event("25th December 2021", "Christmas", "1000", "The North Pole", "Drunk Elves Rockin' Around The Christmas Tree")
+event4 = Event("17th March 2021", "St Patricks Day", "500", "Function Hall", "Wear Green" )
+event5 = Event("11th September 2021", "E50 Graduation", "25", "Castle Street, Edinburgh", "Prepare to Celebrate")
+events = [event1, event2, event3, event4, event5]
